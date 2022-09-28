1c1def5c025f85ce85b7cd3158fdd6d690e1d692
Aakash Verma <dev.aakashv@gmail.com>
2022-09-28 13:26:41 +0530

    format `SubscriptionSection.js` using vscode

--- src/components/SubscriptionSection/SubscriptionSection.js
+++ src/components/SubscriptionSection/SubscriptionSection.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { SubscriptionWrapper, SubsWrapperLevelOne, SubsWrapperLevelTwo } from '../../styles/SubscriptionSection.styled';
-import { MainHeading, Para, Span, Button } from '../../bem/BEM.styled';
+import React from "react";
+import {
+  SubscriptionWrapper,
+  SubsWrapperLevelOne,
+  SubsWrapperLevelTwo,
+} from "../../styles/SubscriptionSection.styled";
+import { MainHeading, Para, Span, Button } from "../../bem/BEM.styled";
 
 const SubscriptionSection = (props) => {
-    return (
-        <SubscriptionWrapper>
-            <MainHeading className="heading">Monthly Subscription</MainHeading>
-            <SubsWrapperLevelOne>
-                <SubsWrapperLevelTwo>
-                    <Para className='price'>$29 </Para>
-                    <Span className='renewal__time'>per month</Span>
-                </SubsWrapperLevelTwo>
-                <Para className='tag__line'>Full access for less than $1 a day</Para>
-            </SubsWrapperLevelOne>
-            <Button>Sign Up</Button>
-        </SubscriptionWrapper>
-    );
-}
+  return (
+    <SubscriptionWrapper>
+      <MainHeading className="heading">Monthly Subscription</MainHeading>
+      <SubsWrapperLevelOne>
+        <SubsWrapperLevelTwo>
+          <Para className="price">$29 </Para>
+          <Span className="renewal__time">per month</Span>
+        </SubsWrapperLevelTwo>
+        <Para className="tag__line">Full access for less than $1 a day</Para>
+      </SubsWrapperLevelOne>
+      <Button>Sign Up</Button>
+    </SubscriptionWrapper>
+  );
+};
 
 export default SubscriptionSection;
