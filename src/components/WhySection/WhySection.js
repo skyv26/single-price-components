86813fca580c74c4c9e84a7c1ce7052a9f50caf8
Aakash Verma <dev.aakashv@gmail.com>
2022-09-28 13:27:01 +0530

    format `WhySection.js` using vscode

--- src/components/WhySection/WhySection.js
+++ src/components/WhySection/WhySection.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { WhyWrapper, UnorderedList, List } from '../../styles/WhySection.styled';
-import { MainHeading } from '../../bem/BEM.styled';
+import React from "react";
+import {
+  WhyWrapper,
+  UnorderedList,
+  List,
+} from "../../styles/WhySection.styled";
+import { MainHeading } from "../../bem/BEM.styled";
 
 const WhySection = (props) => {
-    return (
-        <WhyWrapper>
-            <MainHeading className='heading'>Why Us</MainHeading>
-            <UnorderedList>
-                <List>Tutorials by industry experts</List>
-                <List>Peer &amp; expert code review</List>
-                <List>Coding exercises</List>
-                <List>Access to our GitHub repos</List>
-                <List>Community forum</List>
-                <List>Flashcard decks</List>
-                <List>New videos every week</List>
-            </UnorderedList>
-        </WhyWrapper>
-    );
-}
+  return (
+    <WhyWrapper>
+      <MainHeading className="heading">Why Us</MainHeading>
+      <UnorderedList>
+        <List>Tutorials by industry experts</List>
+        <List>Peer &amp; expert code review</List>
+        <List>Coding exercises</List>
+        <List>Access to our GitHub repos</List>
+        <List>Community forum</List>
+        <List>Flashcard decks</List>
+        <List>New videos every week</List>
+      </UnorderedList>
+    </WhyWrapper>
+  );
+};
 
 export default WhySection;
